63e88f1cd42947814d1ae5b79038190ad87b6daa
Igor Redchuk <redchuk@gmail.com>
2017-09-05 21:26:00 +0200

    Update packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import babel from 'rollup-plugin-babel'
 
 export default {
-  entry: 'index.js',
-  dest: 'dist/bundle.js',
-  format: 'umd',
-  moduleName: 'ArrayJoin',
+  input: 'index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'umd'
+  },
+  name: 'ArrayJoin',
   plugins: [babel()]
 }
