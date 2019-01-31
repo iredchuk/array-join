24ea868efa58a240ee4a2840161595a07ae5f13f
ired <igor.redchuk@futurice.com>
2019-01-31 09:46:17 +0100

    Add typing tests

---
+++ src/index.d.ts
@@ -0,0 +1,28 @@
+export type MatchFn = (a: object, b: object) => boolean;
+
+export interface Options {
+  key?: string;
+  key1?: string;
+  key2?: string;
+  propMap1?: (prop: string) => string;
+  propMap2?: (prop: string) => string;
+  match?: MatchFn;
+}
+
+export function join(
+  array1: object[],
+  array2: object[],
+  options: Options
+): object[];
+
+export function leftJoin(
+  array1: object[],
+  array2: object[],
+  options: Options
+): object[];
+
+export function fullJoin(
+  array1: object[],
+  array2: object[],
+  options: Options
+): object[];
---
+++ src/index.test-d.ts
@@ -0,0 +1,32 @@
+import { expectType } from 'tsd-check';
+import { join, leftJoin, fullJoin } from '.';
+
+const array1 = [{ a: 1, b: 2 }];
+const array2 = [{ a: 2, c: 3 }];
+
+function testTypes(joinFn: any) {
+  expectType<object[]>(joinFn(undefined, undefined, {}));
+
+  expectType<object[]>(joinFn(array1, array2, { key: 'a' }));
+
+  expectType<object[]>(joinFn(array1, array2, { key1: 'b', key2: 'c' }));
+
+  expectType<object[]>(
+    joinFn(array1, array2, {
+      key: 'a',
+      propMap1: s => s + '1',
+      propMap2: s => s + '2'
+    })
+  );
+
+  expectType<object[]>(
+    joinFn(array1, array2, {
+      key: 'a',
+      match: (left, right) => left['a'] === right['a']
+    })
+  );
+}
+
+testTypes(join);
+testTypes(leftJoin);
+testTypes(fullJoin);
